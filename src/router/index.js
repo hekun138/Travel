1438a51b527573cf3c15ebcc483136616591b336
Hogan <418730859@qq.com>
2020-10-24 00:49:23 +0800

    城市页面搜索框布局

--- src/router/index.js
+++ src/router/index.js
@@ -9,15 +9,15 @@ const routes = [
     path: '/',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/city',
+    name: 'City',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../pages/city/City.vue')
   }
-  // {
-  //   path: '/list',
-  //   name: 'List',
-  //   // route level code-splitting
-  //   // this generates a separate chunk (about.[hash].js) for this route
-  //   // which is lazy-loaded when the route is visited.
-  //   component: () => import(/* webpackChunkName: "about" */ '../pages/list/List.vue')
-  // }
 ]
 
 const router = new VueRouter({
